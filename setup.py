d2c50a71f327c463851a28811b7fc5c3a4565e76
karthick <prog@ecomsense.in>
2022-10-12 16:51:47 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bnsemcx',
+      version='1.0',
+      description='broker agnostic api',
+      author='b karthick',
+      author_email='prog@ecomsense.in',
+      url='https://github.com/pannet1/bnsemcx/',
+      packages=['bnsemcx']
+     )
