0d1d07e74bcd536cf968ccb0d441a96919bb5e39
karthick <prog@ecomsense.in>
2023-06-25 15:24:56 +0530

    add reset time to sleep utils

--- toolkit/utilities.py
+++ toolkit/utilities.py
@@ -5,19 +5,21 @@ from typing import Typing
 class Utilities:
 
     def __init__(self):
-        self.reset()
+        self.t = 1
+        self._reset()
 
-    def reset(self)
+    def _reset(self)
         self.slp = 1
 
     def slp_til_nxt_sec(self):
-        self.t = 1
         secs = dt.now().second
         while secs == dt.now().second:
-            self.t = dt.now().microsecond / 1000000
-            sleep(self.t)
-        self.slp += self.t
+            t = dt.now().microsecond / 1000000
+            print(f"sleeping for {t} seconds")
+            sleep(t)
+        self.slp += t
 
     def slp_for(self, sec=1):
         sleep(sec)
+        self.reset()
 
