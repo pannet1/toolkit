d486e27b79d3ded40b0109f166bad28d7e6f06de
karthick <prog@ecomsense.in>
2023-06-25 14:50:26 +0530

    add sleep utilities

--- toolkit/utilities.py
+++ toolkit/utilities.py
@@ -1,10 +1,23 @@
 from datetime import datetime as dt
 from time import sleep
-
+from typing import Typing
 
 class Utilities:
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self)
+        self.slp = 1
+
     def slp_til_nxt_sec(self):
+        t = 1
         secs = dt.now().second
         while secs == dt.now().second:
-            t = dt.now()
-            sleep(t.microsecond / 1000000)
+            t = dt.now().microsecond / 1000000
+            sleep(t)
+        self.slp += t
+
+    def slp_for(self, sec=1):
+        sleep(sec)
+
