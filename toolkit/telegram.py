235e4c12110f083fc13fccb39e6e8c8a837d8c52
pannet1 <prog@ecomsense.in>
2023-10-30 19:18:14 +0530

    add support for json

--- toolkit/telegram.py
+++ toolkit/telegram.py
@@ -1,5 +1,6 @@
 import requests as r
 from toolkit.fileutils import Fileutils
+from requests.structures import CaseInsensitiveDict
 
 
 base_url = "https://api.telegram.org/"
@@ -19,13 +20,13 @@ class Telegram:
 
     def _set_chat_id(self, fpath: str):
         futils = Fileutils()
-        url = f"{base_url}bot{self.api_key}/getUpdates"
-        print(url)
         if self.chat_id == 0:
             if futils.is_file_exists(f"{fpath}/.telegram"):
                 self.chat_id = futils.read_file(f"{fpath}/.telegram")
 
         if not self.chat_id:
+            url = f"{base_url}bot{self.api_key}/getUpdates"
+            print(url)
             print("message to your bot after /start")
             time = __import__("time")
             time.sleep(60)
@@ -38,8 +39,17 @@ class Telegram:
             if self.chat_id:
                 futils.write_file(f"{fpath}/.telegram", str(self.chat_id))
 
-    def send_msg(self, msg: str):
-        url = f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}&text="
-        resp = r.get(url + msg)
-        if resp:
-            self.last_msg = resp.json().get("result").get("text")
+    def send_msg(self, msg: str | dict):
+        if isinstance(msg, dict):
+            url = f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}"
+            data = {"text": str(msg)}
+            resp = r.post(url, data=data)
+            if resp:
+                self.last_msg = resp.json().get("result").get("text")
+        else:
+            url = (
+                f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}&text="
+            )
+            resp = r.get(url + msg)
+            if resp:
+                self.last_msg = resp.json().get("result").get("text")
