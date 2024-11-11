978e092473ac7545b4b84f17c65cf539a53d3984
prog@ecomsense.in <installer@gmail.com>
2024-11-12 03:17:25 +0530

    filter dict keys

--- toolkit/datastruct.py
+++ toolkit/datastruct.py
@@ -1,13 +1,15 @@
-from typing import Dict
+from typing import Dict, List
 
 
-class Datastruct:
-    def fltr_dct_by_key(self, dct: Dict, lst) -> Dict:
-        """
-        generic function to filter any dict
-        """
-        dct_filter = {}
-        for k, v in dct.items():
-            if k in lst:
-                dct_filter[k] = v
-        return dct_filter
+def filter_dictionary_by_keys(elephant: Dict, keys: List) -> Dict:
+    """
+    generic function to filter any dict
+    """
+    if not any(elephant):
+        return elephant
+
+    dct_filter = {}
+    for k, v in elephant.items():
+        if k in keys:
+            dct_filter[k] = v
+    return dct_filter
