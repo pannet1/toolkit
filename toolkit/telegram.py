c7b5790246d5e6381792830af34a0158d04e7071
pannet1 <prog@ecomsense.in>
2025-06-10 12:21:01 +0530

    fix typing

--- toolkit/telegram.py
+++ toolkit/telegram.py
@@ -1,6 +1,5 @@
 import requests as r
 from toolkit.fileutils import Fileutils
-from requests.structures import CaseInsensitiveDict
 
 
 base_url = "https://api.telegram.org/"
@@ -12,44 +11,42 @@ class Telegram:
     https://www.shellhacks.com/telegram-api-send-message-personal-notification-bot/
     """
 
-    def __init__(self, api_key: str, chat_id: int = 0, fpath: str = "./"):
+    def __init__(self, api_key: str, chat_id: int = 0):
         self.api_key = api_key
         self.chat_id = chat_id
-        self.last_msg = ""
-        self._set_chat_id(fpath)
-
-    def _set_chat_id(self, fpath: str):
-        futils = Fileutils()
+        self._last_msg = ""
         if self.chat_id == 0:
-            if futils.is_file_exists(f"{fpath}/.telegram"):
-                self.chat_id = futils.read_file(f"{fpath}/.telegram")
+            self._set_chat_id()
 
-        if not self.chat_id:
-            url = f"{base_url}bot{self.api_key}/getUpdates"
-            print(url)
-            print("message to your bot after /start")
-            time = __import__("time")
-            time.sleep(60)
-            resp = r.get(url)
-            if resp:
-                self.chat_id = (
-                    resp.json().get("result")[0].get("message").get("chat").get("id")
-                )
-                print(f"going to write chat_id {self.chat_id}")
-            if self.chat_id:
-                futils.write_file(f"{fpath}/.telegram", str(self.chat_id))
-
-    def send_msg(self, msg: str | dict):
-        if isinstance(msg, dict):
-            url = f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}"
-            data = {"text": str(msg)}
-            resp = r.post(url, data=data)
-            if resp:
-                self.last_msg = resp.json().get("result").get("text")
-        else:
-            url = (
-                f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}&text="
+    def _set_chat_id(self):
+        print("add the bot to your group and send a test message")
+        __import__("time").sleep(180)
+        url = f"{base_url}bot{self.api_key}/getUpdates"
+        resp = r.get(url)
+        try:
+            self.chat_id = (
+                resp.json().get("result")[0].get("message").get("chat").get("id")
             )
-            resp = r.get(url + msg)
-            if resp:
-                self.last_msg = resp.json().get("result").get("text")
+        except Exception as e:
+            print(f"Unable to set chat id due to {e}")
+
+    def send_msg(self, msg):
+        try:
+            if isinstance(msg, dict):
+                url = f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}"
+                data = {"text": str(msg)}
+                resp = r.post(url, data=data)
+                if resp:
+                    self._last_msg = resp.json().get("result").get("text")
+            else:
+                url = f"{base_url}bot{self.api_key}/sendMessage?chat_id={self.chat_id}&text="
+                resp = r.get(url + msg)
+                if resp:
+                    self._last_msg = resp.json().get("result").get("text")
+        except Exception as e:
+            print(f"{e} while sending message")
+            self._set_chat_id()
+
+    @property
+    def last_msg(self):
+        return self._last_msg
